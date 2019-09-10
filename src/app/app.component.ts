79f4aa417bc0e690bb0e93207ac9f32c4c1926c1
geethagaddipati <balugaddipati@gmail.com>
2019-09-11 07:58:47 +0800

    comments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+//app.component.ts
 import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
-import { MapsAPILoader, MouseEvent } from '@agm/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { MapsAPILoader } from '@agm/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,19 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
   title: string = 'AGM project';
-  latitude: number;
-  longitude: number;
+  latitude: number; // source latitude 
+  longitude: number; // source longitude
   zoom: number;
-  address: string;
+  address: string; // destination address displaying on html page
   dests: any;
-  destinations: any;
-  radius: number;
-  apiKey: string;
-  currentLocation: string;
-  duration: string;
-  distance: string;
-  origin: any;
-  destination: any;
+  destinations: any[]; // Array of all destination addresses
+  radius: number; // radius within which you want nearby places
+  apiKey: string; // googlemaps api key
+  currentLocation: string; // source current location from where to start
+  duration: string; // duration to travel from source to destination
+  distance: string; // distance between source and destination in kms
+  origin: any; // origin on agm-direction of html to get the direction
+  destination: any; // destination on agm-direction html 
 
   private geoCoder;
 
@@ -40,36 +41,38 @@ export class AppComponent implements OnInit {
     this.duration = "";
     this.origin = {};
     this.destination = {};
-    this.apiKey = "";//Add your generated API key here
+    this.apiKey = "";
   }
-
+  // On selecting radius within which the user wants the destinations
   selectRange(radius: number) {
     if (radius == NaN) {
       this.radius = 10;
     } else {
       this.radius = radius;
     }
-    //getted from binding
+    this.destinations = [];
     // console.log(this.radius * 1000, this.currentLocation)
-    this.getGeoLocation(this.currentLocation);
+    this.getDestinations()
   }
 
+  // On change of the destination get the 
   changeDest(dest: string) {
     this.address = dest;
     var target_lat = "";
     var target_lng = "";
-    console.log('source: ', this.latitude, this.longitude);
+    // console.log('source: ', this.latitude, this.longitude);
     // console.log(this.dests)
-    // var self = this;
-    this.dests.forEach(element => {
+    for (let idx in this.dests) {//.forEach(element => {
+      let element = this.dests[idx];
       if (element['name'] == dest) {
         // console.log(">>>>",element);
         this.distance = element['distance'];
         this.duration = element['duration'];
         target_lat = element['latitude'];
         target_lng = element['longitude'];
+        break;
       }
-    });
+    };
     this.origin = { lat: this.latitude, lng: this.longitude };
     this.destination = { lat: target_lat, lng: target_lng };
   }
@@ -83,77 +86,47 @@ export class AppComponent implements OnInit {
         this.origin = { lat: this.latitude, lng: this.longitude };
         // console.log(this.origin)
         this.zoom = 8;
-        this.getAddress(this.latitude, this.longitude);
+        // console.log(position)
+        this.getAddress();
       });
     }
   }
 
-
-  markerDragEnd($event: MouseEvent) {
-    // console.log($event);
-    this.latitude = $event.coords.lat;
-    this.longitude = $event.coords.lng;
-    this.getAddress(this.latitude, this.longitude);
-  }
-
-  setMarker($event) {
-    // console.log("----",$event, this.insearchRef.nativeElement.value);
-    this.getGeoLocation($event.target.value);
-  }
-
-  getGeoLocation(address: string): void {
-    // console.log('Getting address: ', address);
-    var geocoder = new google.maps.Geocoder();
-    var self = this;
-    geocoder.geocode({ 'address': address }, function (results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        self.latitude = results[0].geometry.location.lat();
-        self.longitude = results[0].geometry.location.lng();
-        // console.log("===",address, results, self.latitude, self.longitude);
-        self.getAddress(self.latitude, self.longitude);
-      }
-    });
-  }
-
-  getAddress(latitude, longitude) {
+  // To get all the destinations from given source
+  getDestinations() {
+    var url = "?location=" + this.latitude + "," + this.longitude + "&radius=" + (this.radius * 1000) + "&keyword=&key=" + this.apiKey;
+    // console.log(url);
     var self = this;
     this.destinations = [];
-    // Get address based on latitude and longitude
-    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
-      // console.log('>>>>>', results[0].formatted_address, latitude, longitude);
-      self.currentLocation = results[0].formatted_address;
-      // new google.maps.places.PlacesService(MapsAPILoader)
-      var url = "?location=" + latitude + "," + longitude + "&radius=" + (self.radius * 1000) + "&keyword=&key=" + this.apiKey;
-      console.log(url)
-      // API call to get all the nearby places
-      this.http.get("/api" + url).subscribe(response => {
-        // on getting successful response from nearby search
-        // console.log("---", response);
-        // self.destinations = [];
-        response['results'].forEach(loc => {
-          // console.log("loc name:",loc['name'])
-          self.destinations.push(loc['geometry']['location'])
-        });
-        var origin = results[0].formatted_address;
 
-        // DistanceMatrixService is to find the distance and traveltime
-        var service = new google.maps.DistanceMatrixService();
-        service.getDistanceMatrix(
-          {
-            origins: [origin],
-            destinations: self.destinations,
-            travelMode: google.maps.TravelMode.DRIVING
-          }, callback);
+    // API call to get all the nearby places
+    this.http.get("/api" + url).subscribe(response => {
+      // on getting successful response from nearby search
+      // console.log("---", response);
+      response['results'].forEach(loc => {
+        this.destinations.push(loc['geometry']['location'])
+      });
 
-        // Success callback
-        function callback(response, status) {
-          // console.log(response);
-          self.dests = [{ 'name': '---- Select Your Destiny ----' }];
-          var count = 1;
+      // DistanceMatrixService is to find the distance and traveltime
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix(
+        {
+          origins: [this.currentLocation],
+          destinations: this.destinations,
+          travelMode: google.maps.TravelMode.DRIVING
+        }, callback);
+
+      // Success callback
+      function callback(response, status) {
+        // console.log(response, status);
+        self.dests = [{ 'name': '---- Select Your Destiny ----' }];
+        // var count = 1;
+        // Check if the data exists
+        if (response['rows'].length) {
           var dest_elements = response['rows'][0]['elements'];
-          for (var idx in dest_elements) {
+          for (let idx in dest_elements) {
             if (dest_elements[idx]['status'] == 'OK' && dest_elements[idx]['distance']['value']) {
-              console.log(response['destinationAddresses'][idx], dest_elements[idx])
+              // console.log(response['destinationAddresses'][idx], dest_elements[idx])
               self.dests.push({
                 'name': response['destinationAddresses'][idx],
                 'distance': dest_elements[idx]['distance']['text'],
@@ -167,22 +140,30 @@ export class AppComponent implements OnInit {
               // } else {
               //   count++;
               // }
+
             }
           }
         }
-      },
-        err => {
-          console.log("POST call in error", err);
-          // callback([method, 403, err])
-        },
-        () => {
-          // console.log("The POST observable is now completed.");
-        });
+      }
+    },
+      err => {
+        console.log("Error in google API", err);
+        // callback([method, 403, err])
+      });
+  }
 
+  // Get the address from the given latitude and longitude 
+  getAddress() {
+    this.destinations = [];
+    // Get address based on latitude and longitude
+    this.geoCoder.geocode({ 'location': { lat: this.latitude, lng: this.longitude } }, (results, status) => {
+      // console.log('>>>>>', results[0].formatted_address, latitude, longitude);
+      this.currentLocation = results[0].formatted_address;
+      // console.log("++++++", this.currentLocation, results);
       if (status === 'OK') {
         if (results[0]) {
           this.zoom = 12;
-          this.address = results[0].formatted_address;
+          this.getDestinations();
         } else {
           window.alert('No results found');
         }
@@ -198,17 +179,33 @@ export class AppComponent implements OnInit {
     this.mapsAPILoader.load().then(() => {
       this.setCurrentLocation();
       this.geoCoder = new google.maps.Geocoder;
+      // var self = this;
       let autocomplete1 = new google.maps.places.Autocomplete(this.insearchRef.nativeElement, {
         types: []
       });
+
+      // On change of the source address which is autocompleted
       autocomplete1.addListener("place_changed", () => {
         this.ngZone.run(() => {
+
           //get the place result
           let place: google.maps.places.PlaceResult = autocomplete1.getPlace();
-          //verify result
+          // console.log(place)
+
+          // verify result
           if (place.geometry === undefined || place.geometry === null) {
             return;
           }
+
+          // Get the source location address, latitude and longitude
+          this.currentLocation = place.formatted_address;
+          console.log(this.currentLocation, place.geometry.location.lat(), place.geometry.location.lng())
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.getDestinations()
+
+          // this.getGeoLocation(self.currentLocation)
+
         });
       });
     });
